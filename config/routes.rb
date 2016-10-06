f8a1eb9293d9d635edbc696f1f71304b23e3cbd5
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-06 14:39:46 +0200

    Gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'firma' => 'static_pages#firma'
