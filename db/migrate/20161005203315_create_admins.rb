ba0adee9459535f55c0c3f3cfbbb3a2740338b6c
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-06 16:02:16 +0200

    Poprawiono model Admin

--- db/migrate/20161005203315_create_admins.rb
+++ db/migrate/20161005203315_create_admins.rb
@@ -1,8 +1,6 @@
 class CreateAdmins < ActiveRecord::Migration[5.0]
   def change
     create_table :admins do |t|
-      t.string :email
-      t.string :password_digest
 
       t.timestamps null: false
     end
