fe9512621c51271033901b4ab50f0127492d3b4c
Maciej Lorens <maciej.lorens@gmail.com>
2016-10-13 14:26:33 +0200

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161012193335) do
-
+ActiveRecord::Schema.define(version: 20161013082254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +28,10 @@ ActiveRecord::Schema.define(version: 20161012193335) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.integer "project_id"
+    t.integer  "project_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.index ["project_id"], name: "index_resources_on_project_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,9 +45,10 @@ ActiveRecord::Schema.define(version: 20161012193335) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-
-    t.integer  "sign_in_count"
-
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
