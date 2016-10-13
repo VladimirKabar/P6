aeb6e3422f6aece0e2d8cf9863b3b302bd7aa410
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-13 14:18:35 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -47,14 +47,7 @@ ActiveRecord::Schema.define(version: 20161012193335) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-<<<<<<< HEAD
-    t.integer  "sign_in_count"
-=======
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
->>>>>>> 670c29544bed3bd898507280617dc3b50db6f148
+
   end
 
 end
