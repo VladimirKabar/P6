af07f4056230230113ff42799f091f5ded193961
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-18 20:20:06 +0200

    kolejne poprawki w js i wygladzie

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
 
-
-  get 'resources/index'
-
-  get 'resources/show'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root 'static_pages#home'
-  get 'about' => 'static_pages#about'
   get 'firma' => 'static_pages#firma'
-  get 'projekty' => 'static_pages#projekty'
   get 'kontakt' => 'static_pages#kontakt'
   get 'gen' => 'static_pages#test'
   get 'test' => 'static_pages#index1'
