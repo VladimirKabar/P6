2e00a611ecf38b7ebaca6b2137c14bc39945a30f
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-19 20:58:13 +0200

    kolejne poprawki

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20161013082254) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
-    t.string   "type"
+    t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
