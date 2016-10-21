286cb5d50fc6178d9abea7e346a20f0454ba45e1
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-21 18:04:15 +0200

    fixings

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,25 @@
 
 admin = User.create!(email: 'wizzart@gmail.com', password: 'haslo123')
 
-projekt1 = Project.create!(title: "Balkon", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
-projekt2 = Project.create!(title: "kuchnia", category: "projekt", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
-projekt3 = Project.create!(title: "lazienka", category: "projekt", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
-projekt4 = Project.create!(title: "dom", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p1 = Project.create!(title: "Balkon", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p2 = Project.create!(title: "kuchnia", category: "projekt", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p3 = Project.create!(title: "lazienka", category: "projekt", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p4 = Project.create!(title: "dom", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p5 = Project.create!(title: "dom", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p6 = Project.create!(title: "dom", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p7 = Project.create!(title: "dom", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
+p8 = Project.create!(title: "dom", category: "wizualizacje", description: " I tak powstał bardzo długiiii i interesujucy opis tego wielkiego i wyjatkowego projektu albo w sumie czokapik")
 
-4.times do |x|
+x=0
+[p1, p2, p3, p4,p5,p6,p7,p8].each do |project|
   4.times do |y|
     title = "jakis tam tytul #{y+1}"
     description = "jakis tam opis #{y+1}"
-    if (x==0)
-      @project = projekt1
-    else
-      if (x==1)
-        @project = projekt2
-      else
-        if (x==2)
-          @project = projekt3
-        else
-          if (x==3)
-            @project = projekt4
-          end
-        end
-      end
-    end
     Resource.create!(title: title,
                      image: File.new(File.join(Rails.root, "/app/assets/test/#{x+y+1}.jpg")),
                      description: description,
-                     project: @project)
+                     project: project)
+
   end
+  x += 1
 end
