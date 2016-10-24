9f6db459c567e768193fadf094e3e524e4050bd2
Vladimir Kabar <vladimirkabar@gmail.com>
2016-10-24 13:38:22 +0200

    PR fix2

--- db/migrate/20161012193057_create_projects.rb
+++ db/migrate/20161012193057_create_projects.rb
@@ -1,14 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
-<<<<<<< HEAD
-      t.string :title,              null: false
-      t.string :category,              null: false
-=======
+
       t.string :title, null:false
       t.string :category, null:false
       t.string :description
->>>>>>> master
 
       t.timestamps
     end
