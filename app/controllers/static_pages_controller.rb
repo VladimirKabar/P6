10f954335f86413bd93954b3e0ec042b0f847131
Vladimir Kabar <vladimirkabar@gmail.com>
2016-11-23 21:14:50 +0100

    fixings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,11 +10,7 @@ class StaticPagesController < ApplicationController
   end
 
   def mail
-    @name = params[:name]
-    @email = params[:email]
-    @content = params[:content]
-    puts @name
-    MailerNotifierMailer.mail_to_me(@name,@email,@content).deliver
+    MailerNotifierMailer.mail_to_me(params[:name],params[:email],params[:content]).deliver
     redirect_to contact_path, notice: "Twoja wiadomość została wysłana"
   end
 
