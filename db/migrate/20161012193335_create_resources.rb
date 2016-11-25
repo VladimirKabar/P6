83b373e7ed0641f590f08e459f545ba0f18228bc
Vladimir Kabar <vladimirkabar@gmail.com>
2016-11-25 22:43:46 +0100

    many many fixs

--- db/migrate/20161012193335_create_resources.rb
+++ db/migrate/20161012193335_create_resources.rb
@@ -1,8 +1,6 @@
 class CreateResources < ActiveRecord::Migration[5.0]
   def change
     create_table :resources do |t|
-      t.string :title
-      t.text :description
       t.attachment :image
       t.references :project
 
