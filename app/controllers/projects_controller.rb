3939d06af577e91da9c7d653937a60b5c17d70f1
Maciej Lorens <maciej.lorens@gmail.com>
2017-01-17 17:39:00 +0100

    add position to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.includes(:resources).filter(params[:category]).order(created_at: :desc)
+    @projects = Project.includes(:resources).filter(params[:category]).order(position: :asc)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116102312) do
+ActiveRecord::Schema.define(version: 20170117161818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170116102312) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.boolean  "show_on_home", default: false
+    t.integer  "position"
   end
 
   create_table "resources", force: :cascade do |t|
